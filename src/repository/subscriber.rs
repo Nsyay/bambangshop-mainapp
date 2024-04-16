12826bb898aa7cee23df1b0f5eeddc20313bcf80
Nasya Ayu <ayu.siti21@ui.ac.id>
2024-04-16 21:14:01 +0700

    fixed typo in src\repository\subscriber.rs

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -1,38 +1,37 @@
-use dashmap::Dashmap;
+use dashmap::DashMap;
 use lazy_static::lazy_static;
 use crate::model::subscriber::Subscriber;
 pub struct SubscriberRepository;
 
+// Singleton of Databsasee
 lazy_static! {
-    static ref SUBSCRIBERS: DashMap<String, Subscriber>> = DashMap::new();
+    static ref SUBSCRIBERS: DashMap<String, DashMap<String, Subscriber>> = DashMap::new();
 }
 
-impl SubscriberRepository {
-    pub fn add(product_type: &str, subscriber: Subscriber) -> Subscriber {
+impl SubscriberRepository{
+    pub fn add(product_type: &str, subscriber: Subscriber)->Subscriber{
         let subscriber_value = subscriber.clone();
-        if SUBSCRIBERS.get(product_type).is_none() {
+        if SUBSCRIBERS.get(product_type).is_none(){
             SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
         };
 
-        SUBSCRIBERS.get(product_type).unwrap()
-            .insert(subscriber_value.url.clone(), subscriber_value);
+        SUBSCRIBERS.get(product_type).unwrap().insert(subscriber_value.url.clone(),subscriber_value);
         return subscriber;
     }
 
-    pub fn list_all(product_type: &str) -> Vec<Subscriber> {
-        if SUBSCRIBERS.get(product_type).is_none() {
-            SUBSCRIBERS.insert(String::from(profuct_type), DashMap::new());
+    pub fn list_all(product_type: &str)-> Vec<Subscriber>{
+        if SUBSCRIBERS.get(product_type).is_none(){
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
         };
 
-        return SUBSCRIBERS.get(product_type).unwrap().iter()
-            .map(|f| f.value().clone()).collect();
+        return SUBSCRIBERS.get(product_type).unwrap().iter().map(|f| f.value().clone()).collect();
     }
 
-    pub fn delete(product_type: &str, url: &str) -> Option<Subscriber> {
+    pub fn delete(product_type: &str, url:&str)-> Option<Subscriber>{
         if SUBSCRIBERS.get(product_type).is_none(){
             SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
         }
-        let result  = SUBSCRIBERS.get(product_type).unwrap().remove(url);
+        let result = SUBSCRIBERS.get(product_type).unwrap().remove(url);
         if !result.is_none(){
             return Some(result.unwrap().1);
         }
